b1ac11bb1f98cad9946604c58329c87096c0f7c3
Leoni Kaian <leonikfcdegois@gmail.com>
2023-10-02 18:32:13 -0300

    components e styles criados. Placeholders dizem muito sobre mim

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { MiniCardComponent } from './components/mini-card/mini-card.component';
+import { BlogTitleComponent } from './components/blog-title/blog-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CardComponent,
+    MiniCardComponent,
+    BlogTitleComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
